373eacadcea736e001ad85434ceb49f9af41c90c
Dinis Machado <dimachad@student.42berlin.de>
2025-01-23 00:27:39 +0100

    Add exception read buffer with end of line

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,37 @@
 /*   By: dimachad <dimachad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:16:11 by dimachad          #+#    #+#             */
-/*   Updated: 2025/01/20 18:21:33 by dimachad         ###   ########.fr       */
+/*   Updated: 2025/01/22 23:37:55 by dimachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 static char	*ft_putstr(char *str)
 {
 	while (*str)
 		write(1, str++, 1);
 	write(1, "\n", 1);
-	return(str);
+	return (str);
 }
 
-char	*ft_strchr(const char *s, int c)
+static char	*ft_strchr(char *s, int c, size_t *char_read)
 {
+	const char	*init_str;
+
+	init_str = s;
 	while (*s != (char)c && *s)
 		s++;
 	if ((char)c == *s)
-		return ((char *)s);
+	{
+		*char_read = (size_t)(s - init_str);
+		return ((char *)++s);
+	}
 	return (NULL);
 }
 
-static char *ft_strcat(char *line_buffer, size_t *line_size, char *read_buffer, size_t  char_read)
+static char	*ft_strcat(char *line_buffer, size_t *line_size, char *read_buffer, size_t char_read)
 {
 	char	*cat_line;
 	size_t	i_line;
@@ -39,14 +46,12 @@ static char *ft_strcat(char *line_buffer, size_t *line_size, char *read_buffer, 
 	cat_line = (char *)malloc((*line_size + 1) * sizeof(char));
 	if (!cat_line)
 		return (NULL);
-
 	i_line = 0;
 	while (line_buffer && line_buffer[i_line])
 	{
 		cat_line[i_line] = line_buffer[i_line];
 		i_line++;
 	}
-
 	i_buffer = 0;
 	while (i_buffer <= char_read)
 	{
@@ -60,11 +65,11 @@ static char *ft_strcat(char *line_buffer, size_t *line_size, char *read_buffer, 
 
 char	*get_next_line(int fd)
 {
-	char	*line_buffer;
-	size_t	char_read;
+	char		*line_buffer;
+	size_t		char_read;
 	static char	*next_line;
-	char	read_buffer[BUFFER_SIZE];
-	size_t	line_size;
+	char		read_buffer[BUFFER_SIZE];
+	size_t		line_size;
 
 	line_size = 0;
 	line_buffer = NULL;
@@ -76,15 +81,10 @@ char	*get_next_line(int fd)
 		char_read = read(fd, read_buffer, BUFFER_SIZE);
 		if ((int)char_read == -1)
 			return (ft_putstr("Error reading file"));
-		next_line = ft_strchr(read_buffer, '\n');
-
-		if (next_line)
-			ft_putstr(next_line);
-
+		next_line = ft_strchr(read_buffer, '\n', &char_read);
 		line_buffer = ft_strcat(line_buffer, &line_size, read_buffer, char_read);
 		if (next_line)
 			return (line_buffer);
-		ft_putstr(line_buffer);
 	}
 	return (line_buffer);
 }
